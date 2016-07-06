da25eba3dd1c5bf6d586b64d9f0dfc27ae7c431a
yipianfengye <1161944036@qq.com>
2016-07-06 15:11:39 +0800

    使用overridePendingTransition的方式实现activity的切换动画

--- app/src/main/java/uuch/com/android_activityanim/MainActivity.java
+++ app/src/main/java/uuch/com/android_activityanim/MainActivity.java
@@ -1,13 +1,43 @@
 package uuch.com.android_activityanim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        initListener();
+    }
+
+    /**
+     * 实现组件的初始化操作
+     */
+    private void initView() {
+        button1 = (Button) findViewById(R.id.button1);
+    }
+
+    /**
+     * 实现组件的初始化监听
+     */
+    private void initListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left);
+            }
+        });
     }
 }
